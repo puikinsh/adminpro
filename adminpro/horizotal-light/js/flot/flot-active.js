865ed982ff403eb2dfb19c6fb30d5b379b0ba741
Aigars Silkalns <aigars.silkalns@gmail.com>
2018-10-15 15:49:33 +0300

    inital release

---
+++ adminpro/horizotal-light/js/flot/flot-active.js
@@ -0,0 +1,94 @@
+(function ($) {
+ "use strict";
+
+            var data1 = [
+                [0,4],[1,8],[2,30],[3,10],[4,4],[5,16],[6,5],[7,19],[8,6],[9,11],[10,30],[11,10],[12,13],[13,4],[14,40],[15,3],[16,6]
+            ];
+            var data2 = [
+                [0,1],[1,0],[2,2],[3,0],[4,1],[5,3],[6,1],[7,5],[8,16],[9,3],[10,2],[11,1],[12,0],[13,2],[14,2],[15,0],[16,0]
+            ];
+            $("#flot-dashboard-chart").length && $.plot($("#flot-dashboard-chart"), [
+                data1, data2
+            ],
+                    {
+                        series: {
+                            lines: {
+                                show: false,
+                                fill: true
+                            },
+                            splines: {
+                                show: true,
+                                tension: 0.4,
+                                lineWidth: 1,
+                                fill: 0.4
+                            },
+                            points: {
+                                radius: 0,
+                                show: true
+                            },
+                            shadowSize: 2
+                        },
+                        grid: {
+                            hoverable: true,
+                            clickable: true,
+                            tickColor: "#d5d5d5",
+                            borderWidth: 1,
+                            color: '#d5d5d5'
+                        },
+                        colors: ["#03a9f4", "#303030"],
+                        xaxis:{
+                        },
+                        yaxis: {
+                            ticks: 4
+                        },
+                        tooltip: false
+                    }
+            );
+
+		 var doughnutData = {
+                labels: ["Logo","Banner","Card" ],
+                datasets: [{
+                    data: [300,50,100],
+                    backgroundColor: ["#a3e1d4","#dedede","#9CC3DA"]
+                }]
+            } ;
+
+
+            var doughnutOptions = {
+                responsive: false,
+                legend: {
+                    display: false
+                }
+            };
+
+
+            var ctx4 = document.getElementById("doughnutChart").getContext("2d");
+            new Chart(ctx4, {type: 'doughnut', data: doughnutData, options:doughnutOptions});
+
+            var doughnutData = {
+                labels: ["App","Software","Game" ],
+                datasets: [{
+                    data: [70,27,85],
+                    backgroundColor: ["#a3e1d4","#dedede","#9CC3DA"]
+                }]
+            } ;
+
+
+            var doughnutOptions = {
+                responsive: false,
+                legend: {
+                    display: false
+                }
+            };
+
+
+            var ctx4 = document.getElementById("doughnutChart2").getContext("2d");
+            new Chart(ctx4, {type: 'doughnut', data: doughnutData, options:doughnutOptions});
+
+
+
+
+
+ 
+})(jQuery); 
+
---
+++ adminpro/material-design-collapse/js/flot/flot-active.js
@@ -0,0 +1,51 @@
+(function ($) {
+ "use strict";
+
+            var data1 = [
+                [0,4],[1,8],[2,30],[3,10],[4,4],[5,16],[6,5],[7,19],[8,6],[9,11],[10,30],[11,10],[12,13],[13,4],[14,40],[15,3],[16,6]
+            ];
+            var data2 = [
+                [0,1],[1,0],[2,2],[3,0],[4,1],[5,3],[6,1],[7,5],[8,16],[9,3],[10,2],[11,1],[12,0],[13,2],[14,2],[15,0],[16,0]
+            ];
+            $("#flot-dashboard-chart").length && $.plot($("#flot-dashboard-chart"), [
+                data1, data2
+            ],
+                    {
+                        series: {
+                            lines: {
+                                show: false,
+                                fill: true
+                            },
+                            splines: {
+                                show: true,
+                                tension: 0.4,
+                                lineWidth: 1,
+                                fill: 0.4
+                            },
+                            points: {
+                                radius: 0,
+                                show: true
+                            },
+                            shadowSize: 2
+                        },
+                        grid: {
+                            hoverable: true,
+                            clickable: true,
+                            tickColor: "#fff",
+                            borderWidth: 1,
+                            color: '#fff'
+                        },
+                        colors: ["#03a9f4", "#000"],
+                        yaxis: {tickLength:0}, 
+						xaxis: {tickLength:0},
+                        tooltip: false
+                    }
+            );
+
+
+			
+
+
+ 
+})(jQuery); 
+
